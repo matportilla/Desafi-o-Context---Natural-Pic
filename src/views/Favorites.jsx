c11183886fcd0b2692aa1c41765418956c9a226a
Nicoarratia <nicolas.andres.arratia@gmail.com>
2023-10-07 20:12:56 -0300

    add favorites views

--- src/views/Favorites.jsx
+++ src/views/Favorites.jsx
@@ -1,1 +1,16 @@
+import React from 'react';
+import { Container, Row } from 'react-bootstrap';
+import Gallery from '../components/Gallery';
 
+const Favorites = () => {
+  return (
+    <Container>
+      <h1 className='py-3'>Favoritos</h1>
+      <Row xs={1} md={2} lg={4} className="g-3 mb-3">
+        <Gallery filtro={'favoritos'} />
+      </Row>
+    </Container>
+  );
+};
+
+export default Favorites;
